8ea5560d99a0258efed382a7165f0a378ddf20ed
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-02-10 23:00:07 -0500

    not give invalid flint

--- src/main/java/net/pufferlab/primal/blocks/BlockStoneGravel.java
+++ src/main/java/net/pufferlab/primal/blocks/BlockStoneGravel.java
@@ -1,11 +1,14 @@
 package net.pufferlab.primal.blocks;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.pufferlab.primal.Config;
 import net.pufferlab.primal.Registry;
 import net.pufferlab.primal.utils.StoneType;
@@ -29,6 +32,24 @@ public class BlockStoneGravel extends BlockMetaGravel {
         }
 
         return random.nextInt(10 - fortune * 3) == 0 ? Items.flint : Item.getItemFromBlock(this);
+    }
+
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+
+        int count = quantityDropped(metadata, fortune, world.rand);
+        for (int i = 0; i < count; i++) {
+            Item item = getItemDropped(metadata, world.rand, fortune);
+            int damage = damageDropped(metadata);
+            if (item == Items.flint) {
+                damage = 0;
+            }
+            if (item != null) {
+                ret.add(new ItemStack(item, 1, damage));
+            }
+        }
+        return ret;
     }
 
     @Override
