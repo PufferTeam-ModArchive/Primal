904f7cadcbdc9c8a1e10f532d14276599e9c46a3
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-04-20 12:34:31 -0400

    prevents water... from doing weird shit

--- src/main/java/net/pufferlab/primal/blocks/BlockFluidPrimal.java
+++ src/main/java/net/pufferlab/primal/blocks/BlockFluidPrimal.java
@@ -1,9 +1,11 @@
 package net.pufferlab.primal.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 import net.pufferlab.primal.Primal;
@@ -29,6 +31,45 @@ public class BlockFluidPrimal extends BlockFluidClassic implements IPrimalBlock 
         flowingIcon = register.registerIcon(Primal.MODID + ":" + this.name + "_flow");
         this.getFluid()
             .setIcons(stillIcon, flowingIcon);
+    }
+
+    @Override
+    public boolean canDisplace(IBlockAccess world, int x, int y, int z)
+    {
+        if (world.getBlock(x, y, z).isAir(world, x, y, z)) return true;
+
+        Block block = world.getBlock(x, y, z);
+
+        if (block == this)
+        {
+            return false;
+        }
+
+        if (displacements.containsKey(block))
+        {
+            return displacements.get(block);
+        }
+
+        Material material = block.getMaterial();
+        if (material.blocksMovement() || material == Material.portal || material == Material.water || material == Material.lava)
+        {
+            return false;
+        }
+
+        int density = getDensity(world, x, y, z);
+        if (density == Integer.MAX_VALUE)
+        {
+            return true;
+        }
+
+        if (this.density > density)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     @Override
