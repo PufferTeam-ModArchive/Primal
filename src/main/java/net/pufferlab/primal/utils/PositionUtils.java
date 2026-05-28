aa18d9e1b00f6e2f223798e8020b0af022ee660c
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-05-27 23:29:35 -0400

    make coord packer it more readable

--- src/main/java/net/pufferlab/primal/utils/PositionUtils.java
+++ src/main/java/net/pufferlab/primal/utils/PositionUtils.java
@@ -2,26 +2,48 @@ package net.pufferlab.primal.utils;
 
 public class PositionUtils {
 
+    // Coordinates Bits
+    public static final int bx = 26; // 26 bits [-33,554,432 to 33,554,432]
+    public static final int by = 12; // 12 bits [-2048 to 2048]
+    public static final int bz = 26; // 26 bits [-33,554,432 to 33,554,432]
+
+    public static final int bzs = 0;
+    public static final int bys = bz;
+    public static final int bxs = bz + by;
+
+    public static final long xm = (1L << bx) - 1L;
+    public static final long ym = (1L << by) - 1L;
+    public static final long zm = (1L << bz) - 1L;
+
+    public static final long cm = 0xFFFFFFFFL;
+    public static final int czs = 32;
+
     public static long packCoord(int x, int y, int z) {
-        long lx = x & 0x3FFFFFFL; // 26 bits [-33,554,432 to 33,554,432]
-        long ly = y & 0xFFFL; // 12 bits [-2048 to 2048]
-        long lz = z & 0x3FFFFFFL; // 26 bits [-33,554,432 to 33,554,432]
-        return (lx << 38) | (ly << 26) | lz;
+
+        long lx = x & xm;
+        long ly = y & ym;
+        long lz = z & zm;
+
+        return (lx << bxs) | (ly << bys) | (lz << bzs);
     }
 
     public static long packChunkCoord(int x, int z) {
-        return (long) x & 4294967295L | ((long) z & 4294967295L) << 32;
+
+        return ((long) x & cm) | (((long) z & cm) << czs);
     }
 
     public static int unpackX(long packed) {
-        return (int) (packed << 0 >> 38); // signed
+
+        return (int) (packed << (64 - bx - bxs) >> (64 - bx));
     }
 
     public static int unpackY(long packed) {
-        return (int) (packed << 26 >> 52); // signed
+
+        return (int) (packed << (64 - by - bys) >> (64 - by));
     }
 
     public static int unpackZ(long packed) {
-        return (int) (packed << 38 >> 38); // signed
+
+        return (int) (packed << (64 - bz - bzs) >> (64 - bz));
     }
 }
