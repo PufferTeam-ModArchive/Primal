bdc0c17d491db514ff2da4ea208f1311021afb29
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-05-31 15:46:56 -0400

    replace each version

--- src/main/java/net/pufferlab/primal/mixins/early/minecraft/client/MixinResourcePackRepository.java
+++ src/main/java/net/pufferlab/primal/mixins/early/minecraft/client/MixinResourcePackRepository.java
@@ -1,7 +1,6 @@
 package net.pufferlab.primal.mixins.early.minecraft.client;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +8,7 @@ import net.minecraft.client.resources.ResourcePackRepository;
 import net.pufferlab.primal.Primal;
 import net.pufferlab.primal.utils.IOUtils;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,19 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ResourcePackRepository.class)
 public class MixinResourcePackRepository {
 
-    @Shadow
-    @Final
-    private File dirResourcepacks;
-    @Shadow
-    @Final
-    protected static FileFilter resourcePackFilter;
-
     @Inject(method = "getResourcePackFiles", at = @At("RETURN"), cancellable = true)
     private void getResourcePackFiles$primal(CallbackInfoReturnable<List<File>> cir) {
 
         List<File> list = new ArrayList<>(cir.getReturnValue());
 
-        File temp = IOUtils.createResourceStreamFile("/" + Primal.textureFile + ".zip", Primal.textureFile, "tmp");
+        File temp = IOUtils.createResourceStreamFile("/" + Primal.textureFile + ".zip", Primal.textureFile + "-" + Primal.VERSION, "tmp");
 
         if (temp != null) {
             list.add(temp);
